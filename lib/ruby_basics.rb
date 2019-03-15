12e13a871143e7a7ef9a19b4d0c2e410b3ef00f6
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 15:21:17 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+   num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue (arg)
+  arg
 end
 
-def greeting
-
+def greeting (greeting, name)
+  
 end
 
 def return_a_value
-
+  puts "Nice"
 end
 
 def last_evaluated_value
-
+    puts "expert"
 end
 
-def pizza_party
-
+def pizza_party (toppings = "cheese")
+  return toppings
 end
