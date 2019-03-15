0f52f24aa674a2fde89c2f83ec3299e2ad373a9f
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 15:26:34 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+   num1 / num2
 end
 
 def assign_variable(value)
+  value = "Bob"
+end
+
+def argue (arg)
+  arg
+end
+
+def greeting (greeting, name)
   
 end
 
-def argue
-  
-end
-
-def greeting
+def return_a_value ()
+  return "Nice"
 
 end
 
-def return_a_value
+def last_evaluated_value ()
+  return "expert"
 
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party (toppings = "cheese")
+  return toppings
 end
